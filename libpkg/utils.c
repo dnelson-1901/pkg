56949dace8947fb4960e9f3ddf63974451e3148b
Dan Nelson <dnelson_1901@yahoo.com>
2025-05-25 11:13:34 -0500

    Add checks to prevent bsearch() from being called on unsorted vec_t's
    Also replace vec_push() with charv_insert_sorted() in places where the vec_t
    does get bsearch()'ed

--- libpkg/utils.c
+++ libpkg/utils.c
@@ -1134,6 +1134,7 @@ charv_search(charv_t *v, const char *el)
 {
 	if (v->len == 0)
 		return (NULL);
+	assert(v->unsorted == false);
 	const char **res = bsearch(&el, v->d, v->len, sizeof(char *), char_cmp);
 	if (res == NULL)
 		return (NULL);
